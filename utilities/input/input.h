2bd91716898a5386b60aaccb80bb04eb30f5db46
jlye <justin.lye@ansys.com>
2021-12-03 17:43:09 -0600

    Use messages::errors in input

--- utilities/input/input.h
+++ utilities/input/input.h
@@ -1,14 +1,11 @@
 #include <vector>
 
 #include "input_stream.h"
+#include "utilities/messages/errors.h"
 
 namespace aoc {
 namespace utilities {
     namespace input {
-        namespace detail {
-            void throw_nonexisting_file(const char* file_name);
-            void throw_corrupted_data(const char* file_name);
-        }
         template<class T>
         std::vector<T> read_file(const char* file_name);
     }
@@ -18,13 +15,13 @@ template<class T>
 std::vector<T> aoc::utilities::input::read_file(const char* file_name) {
     InputStream<T> stream(file_name);
     if (stream.fail()) {
-        detail::throw_nonexisting_file(file_name);
+        messages::errors::throw_nonexistant_file(file_name);
     }
     std::vector<T> file_content;
     while (stream.good()) {
         auto input = stream.get();
         if (stream.fail()) {
-            detail::throw_corrupted_data(file_name);
+            messages::errors::throw_corrupt_data(file_name);
         }
         file_content.push_back(input);
     }
