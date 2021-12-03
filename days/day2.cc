8a45bb6bdc0ac3ef0bec1f189cd56dc301c84fb5
jlye <justin.lye@ansys.com>
2021-12-03 17:47:04 -0600

    Use input file stream in day2.

--- days/day2.cc
+++ days/day2.cc
@@ -3,13 +3,25 @@
 #include <stdexcept>
 
 #include "utilities/data/position.h"
-#include "utilities/input/input.h"
+#include "utilities/messages/errors.h"
+#include "utilities/input/input_stream.h"
 
 int aoc::days::day2::part1(int argc, char* argv[]) {
+    using namespace utilities::input;
+    using namespace utilities::messages;
+    using namespace utilities::data;
+
     try {
-        auto deltas = aoc::utilities::input::read_file<aoc::utilities::data::PositionDelta>(argv[1]);
-        aoc::utilities::data::Position p{ 0,0 };
-        for (const auto& delta : deltas) {
+        InputStream<PositionDelta> stream(argv[1]);
+        if (stream.fail()) {
+            errors::throw_nonexistant_file(argv[1]);
+        }
+        Position p{ 0,0 };
+        while (stream.good()) {
+            auto delta = stream.get();
+            if (stream.fail()) {
+                errors::throw_corrupt_data(argv[1]);
+            }
             p += delta;
         }
         std::cout << p.depth * p.horizontal << std::endl;
@@ -23,13 +35,24 @@ int aoc::days::day2::part1(int argc, char* argv[]) {
 }
 
 int aoc::days::day2::part2(int argc, char* argv[]) {
+    using namespace utilities::input;
+    using namespace utilities::messages;
+    using namespace utilities::data;
+
     try {
-        auto deltas = aoc::utilities::input::read_file<aoc::utilities::data::PositionDelta>(argv[1]);
-        aoc::utilities::data::Orientation p(0);
-        for (const auto& delta : deltas) {
-            p += delta;
+        InputStream<PositionDelta> stream(argv[1]);
+        if (stream.fail()) {
+            errors::throw_nonexistant_file(argv[1]);
         }
-        std::cout << p.depth * p.horizontal << std::endl;
+        Orientation o(0);
+        while (stream.good()) {
+            auto delta = stream.get();
+            if (stream.fail()) {
+                errors::throw_corrupt_data(argv[1]);
+            }
+            o += delta;
+        }
+        std::cout << o.depth * o.horizontal << std::endl;
         return 0;
     } catch (const std::exception& error) {
         std::cerr << error.what() << std::endl;
