a32829b6a2fa49e5c421552918deb163dda0ee0a
jlye <justin.lye@ansys.com>
2021-12-03 16:09:34 -0600

    Add clean, run, and rund batch files.

---
+++ clean.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal enableextensions
+
+del /s/f/q build-files > nul
+rmdir /s/q build-files
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+setlocal enableextensions
+
+call build-files\Release\aoc.exe %*
