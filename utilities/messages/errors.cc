a8c9d7a1afecd8c634cb725818f78578baf188fe
jlye <justin.lye@ansys.com>
2021-12-06 15:24:55 -0600

    add additional error messages.

--- utilities/messages/errors.cc
+++ utilities/messages/errors.cc
@@ -15,8 +15,24 @@ void aoc::utilities::messages::errors::throw_corrupt_data(const char* file_path)
     throw std::runtime_error(ss.str().c_str());
 }
 
+void aoc::utilities::messages::errors::throw_corrupt_data() {
+    throw std::runtime_error("Error! Failed to read file because it contains corrupt data.");
+}
+
 void aoc::utilities::messages::errors::throw_invalid_argument_count(int actual, int expected) {
     std::stringstream ss;
     ss << "Error! Invalid number of arguments. Expected \'" << expected << "\' arguments. Received \'" << actual << "\' agruments.";
+    throw std::runtime_error(ss.str().c_str());
+}
+
+void aoc::utilities::messages::errors::throw_invalid_column_count(int actual, int expected) {
+    std::stringstream ss;
+    ss << "Error! Invaild number of columns. Expected \'" << expected << "\' columns. Found \'" << actual << "\' columns.";
+    throw std::runtime_error(ss.str().c_str());
+}
+
+void aoc::utilities::messages::errors::throw_unexpected_end_of_stream() {
+    std::stringstream ss;
+    ss << "Error! Could not read input. Stream ended unexpectedly.";
     throw std::runtime_error(ss.str().c_str());
 }
--- utilities/messages/errors.h
+++ utilities/messages/errors.h
@@ -6,7 +6,10 @@ namespace utilities {
         struct errors {
             static void throw_nonexistant_file(const char* file_path);
             static void throw_corrupt_data(const char* file_path);
+            static void throw_corrupt_data();
             static void throw_invalid_argument_count(int actual, int expected);
+            static void throw_invalid_column_count(int actual, int expected);
+            static void throw_unexpected_end_of_stream();
         };
     }
 }}
